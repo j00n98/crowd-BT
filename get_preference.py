a748c4531cfb51277060fec5d4eb653eebb43e49
j00n98 <124313383+j00n98@users.noreply.github.com>
2024-05-26 17:57:48 +0900

    Update get_preference.py

--- get_preference.py
+++ get_preference.py
@@ -1,32 +1,44 @@
 import numpy as np
 
 def get_preference(Xt, a, b, alg_params):
-    val_at_a = Xt[a]
-    val_at_b = Xt[b]
+    """
+    Determine the preference between two objects based on their values and algorithm parameters.
+
+    Args:
+        Xt (ndarray): Array of values associated with objects.
+        a (int): Index of the first object.
+        b (int): Index of the second object.
+        alg_params (dict): Dictionary containing algorithm parameters, including 'layers' and 'L'.
+
+    Returns:
+        int: Preference value, 1 if a is preferred over b, -1 if b is preferred over a.
+    """
+    val_at_a = Xt[a]  # Value of the first object
+    val_at_b = Xt[b]  # Value of the second object
     
-    layers = alg_params['layers'] * alg_params['L']
-    a_layer = np.argmax(layers > val_at_a)
-    b_layer = np.argmax(layers > val_at_b)
+    layers = alg_params['layers'] * alg_params['L']  # Calculate layer thresholds
+    a_layer = np.argmax(layers > val_at_a)  # Find the layer index for the first object
+    b_layer = np.argmax(layers > val_at_b)  # Find the layer index for the second object
     
-    pref = (val_at_a > val_at_b)
-    if a_layer == b_layer:
-        p = np.exp(-(val_at_a + val_at_b))  # probability of wrong answer
-        r = np.random.rand()
+    pref = (val_at_a > val_at_b)  # Determine initial preference based on values
+    if a_layer == b_layer:  # If both objects are in the same layer
+        p = np.exp(-(val_at_a + val_at_b))  # Probability of wrong answer
+        r = np.random.rand()  # Generate a random number
         if r < p:
-            pref = not pref
+            pref = not pref  # Flip the preference if random number is less than probability
     
     if not pref:
-        pref = -1
+        pref = -1  # Set preference to -1 if b is preferred
     
     return pref
 
 # Example usage:
-# Xt = np.array([...])
+# Xt = np.array([...])  # Array of object values
 # alg_params = {
-#     'layers': ...,
-#     'L': ...
+#     'layers': ...,  # Layer thresholds
+#     'L': ...  # Number of objects
 # }
-# a = ...
-# b = ...
-# pref = get_preference(Xt, a, b, alg_params)
-# print(pref)
+# a = ...  # Index of the first object
+# b = ...  # Index of the second object
+# pref = get_preference(Xt, a, b, alg_params)  # Get the preference
+# print(pref)  # Output the preference
